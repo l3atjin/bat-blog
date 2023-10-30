a669e7fba0be9f1a7dcc9337e660edda68961273
Batjin Lamjav <batjinlamjav@Batjins-MacBook-Pro-4.local>
2023-10-30 16:14:11 -0700

    adding boilerplate for projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import styles from './layout.module.css';
+
+const projects = [
+  {
+    name: 'Project 1',
+    description: 'Description of Project 1.',
+    technologies: 'React, Node.js, MongoDB',
+    // Add more project details as needed
+  },
+  {
+    name: 'Project 2',
+    description: 'Description of Project 2.',
+    technologies: 'Angular, Express, MySQL',
+  },
+  // Add more projects
+];
 
 export default function Projects() {
   return (
-    <div>projects</div>
-  )
+    <section className={styles.projects}>
+      <h2>Projects</h2>
+      <ul className={styles.projectList}>
+        {projects.map((project, index) => (
+          <li key={index} className={styles.projectItem}>
+            <h3>{project.name}</h3>
+            <p>{project.description}</p>
+            <p>Technologies: {project.technologies}</p>
+            {/* Add more project details here */}
+          </li>
+        ))}
+      </ul>
+    </section>
 }
