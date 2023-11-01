1b025d52b39ed87117eb765b71bf12bfc0bb8b20
Batjin Lamjav <l3atjin@gmail.com>
2023-11-01 16:26:06 -0700

    can now upload to s3

--- pages/projects/image-labeler.tsx
+++ pages/projects/image-labeler.tsx
@@ -1,10 +1,14 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import Image from 'next/image'
+// import { SECRET_KEY } from '@env'
+// import { ACCESS_KEY_ID } from '@env'
 
 export default function ImageLabeler() {
   const [image, setImage] = useState(null);
   const [imageUrl, setImageURL] = useState("");
+
+  
 
   const onImageChange = (e) => {
     setImage(e.target.files[0]);
@@ -15,8 +19,43 @@ export default function ImageLabeler() {
       const newURL = URL.createObjectURL(image);
       console.log("new URL", newURL);
       setImageURL(newURL);
+
+      // upload to AWS
+      
+      const bucketRegion = "us-east-2"
+      const bucketName = "image-labeler-demo"
+      const IdentityPoolId = "us-east-2:3b086e39-d442-4ab0-9f97-ca03cfa89d0f"
+      const AWS = require('aws-sdk');
+
+      AWS.config.update({
+        region: bucketRegion,
+        credentials: new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: IdentityPoolId
+          })
+      });
+
+      var s3 = new AWS.S3({
+          apiVersion: '2006-03-01',
+          params: {Bucket: bucketName}
+      });
+    
+      const params = {
+        Bucket: bucketName,
+        Key: image.name,
+        Body: image
+      };
+      
+      s3.upload(params, (err, data) => {
+        if (err) {
+          console.error('Error uploading image: ', err);
+        } else {
+          console.log('Image uploaded successfully. Image URL:', data.Location);
+        }
+      });
     }
   }, [image])
+
+  
   
 
   return (
